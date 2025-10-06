ee3e80579849a7fb441042ae1bec8540f78eb7a4
Ben Lewis <blewis@hirundo.io>
2025-10-06 10:39:00 +0100

    Fix missing handling of datasets with only archived runs

--- scripts/cleanup_test_artifacts.py
+++ scripts/cleanup_test_artifacts.py
@@ -5,7 +5,7 @@ from typing import Union
 
 import requests
 from hirundo import GitRepo, QADataset, StorageConfig
-from hirundo.dataset_qa import HirundoError, RunStatus
+from hirundo.dataset_qa import HirundoError, QADatasetOut, RunStatus
 from hirundo.logger import get_logger
 from hirundo.storage import ResponseStorageConfig
 
@@ -15,15 +15,20 @@ logger = get_logger(__name__)
 def _delete_dataset(
     dataset_id: int,
     storage_config: Union[StorageConfig, ResponseStorageConfig, None],
-) -> None:
+    deleted_datasets: set[int],
+    deleted_storage_configs: set[int],
+    deleted_git_repos: set[int],
+) -> tuple[set[int], set[int], set[int]]:
     try:
         QADataset.delete_by_id(dataset_id)
+        deleted_datasets.add(dataset_id)
     except (HirundoError, requests.HTTPError) as exc:
         logger.warning("Failed to delete dataset with ID %s: %s", dataset_id, exc)
 
     if storage_config and storage_config.id is not None:
         try:
             StorageConfig.delete_by_id(storage_config.id)
+            deleted_storage_configs.add(storage_config.id)
         except (HirundoError, requests.HTTPError) as exc:
             logger.warning(
                 "Failed to delete storage config with ID %s: %s", storage_config.id, exc
@@ -37,14 +42,21 @@ def _delete_dataset(
             git_repo_id = storage_config.git.repo.id
             try:
                 GitRepo.delete_by_id(git_repo_id)
+                deleted_git_repos.add(git_repo_id)
             except (HirundoError, requests.HTTPError) as exc:
                 logger.warning(
                     "Failed to delete git repo with ID %s: %s", git_repo_id, exc
                 )
+    return deleted_datasets, deleted_storage_configs, deleted_git_repos
 
 
-def _should_delete_dataset(dataset_runs: list, expiry_date: datetime.datetime) -> bool:
+def _should_delete_dataset(
+    dataset_name: str, dataset_runs: list, expiry_date: datetime.datetime
+) -> bool:
     """Return ``True`` if the dataset should be deleted."""
+
+    if not dataset_name.startswith("TEST-"):
+        return False
 
     if not dataset_runs:
         return False
@@ -57,8 +69,81 @@ def _should_delete_dataset(dataset_runs: list, expiry_date: datetime.datetime) -
     return most_recent_run_time <= expiry_date
 
 
+def _handle_live_runs(
+    live_runs_by_dataset: defaultdict[int, list],
+    datasets: dict[int, QADatasetOut],
+    one_week_ago: datetime.datetime,
+    trying_to_delete_datasets: set[int],
+    archived_runs: set[str],
+    deleted_datasets: set[int],
+    deleted_storage_configs: set[int],
+    deleted_git_repos: set[int],
+) -> tuple[set[int], set[str], set[int], set[int], set[int]]:
+    for dataset_id, dataset_runs in live_runs_by_dataset.items():
+        dataset = datasets.get(dataset_id)
+
+        if dataset and _should_delete_dataset(dataset.name, dataset_runs, one_week_ago):
+            trying_to_delete_datasets.add(dataset_id)
+            for run in dataset_runs:
+                try:
+                    QADataset.archive_run_by_id(run.run_id)
+                    archived_runs.add(run.run_id)
+                except (HirundoError, requests.HTTPError) as exc:
+                    logger.warning(
+                        "Failed to archive run with ID %s: %s", run.run_id, exc
+                    )
+
+            deleted_datasets, deleted_storage_configs, deleted_git_repos = (
+                _delete_dataset(
+                    dataset_id,
+                    dataset.storage_config,
+                    deleted_datasets,
+                    deleted_storage_configs,
+                    deleted_git_repos,
+                )
+            )
+    return (
+        trying_to_delete_datasets,
+        archived_runs,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    )
+
+
+def _handle_archived_runs(
+    archived_runs_by_dataset: defaultdict[int, list],
+    datasets: dict[int, QADatasetOut],
+    one_week_ago: datetime.datetime,
+    trying_to_delete_datasets: set[int],
+    deleted_datasets: set[int],
+    deleted_storage_configs: set[int],
+    deleted_git_repos: set[int],
+) -> tuple[set[int], set[int], set[int], set[int]]:
+    for dataset_id, dataset_runs in archived_runs_by_dataset.items():
+        dataset = datasets.get(dataset_id)
+        if dataset and _should_delete_dataset(dataset.name, dataset_runs, one_week_ago):
+            trying_to_delete_datasets.add(dataset_id)
+            deleted_datasets, deleted_storage_configs, deleted_git_repos = (
+                _delete_dataset(
+                    dataset_id,
+                    dataset.storage_config,
+                    deleted_datasets,
+                    deleted_storage_configs,
+                    deleted_git_repos,
+                )
+            )
+    return (
+        trying_to_delete_datasets,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    )
+
+
 def main() -> None:
-    all_runs = QADataset.list_runs()
+    all_live_runs = QADataset.list_runs()
+    all_archived_runs = QADataset.list_runs(archived=True)
     datasets = {
         dataset_entry.id: dataset_entry
         for dataset_entry in QADataset.list_datasets()
@@ -67,27 +152,66 @@ def main() -> None:
     now = datetime.datetime.now(timezone.utc)
     one_week_ago = now - timedelta(days=7)
 
-    runs_by_dataset: defaultdict[int, list] = defaultdict(list)
-    for run in all_runs:
+    live_runs_by_dataset: defaultdict[int, list] = defaultdict(list)
+    archived_runs_by_dataset: defaultdict[int, list] = defaultdict(list)
+    for run in all_live_runs:
         if run.dataset_id is None or run.run_id is None:
             continue
-        runs_by_dataset[run.dataset_id].append(run)
-
-    for dataset_id, dataset_runs in runs_by_dataset.items():
-        dataset = datasets.get(dataset_id)
-        if dataset is None or not dataset.name.startswith("TEST-"):
+        live_runs_by_dataset[run.dataset_id].append(run)
+    for run in all_archived_runs:
+        if run.dataset_id is None or run.run_id is None:
             continue
+        archived_runs_by_dataset[run.dataset_id].append(run)
 
-        if _should_delete_dataset(dataset_runs, one_week_ago):
-            for run in dataset_runs:
-                try:
-                    QADataset.archive_run_by_id(run.run_id)
-                except (HirundoError, requests.HTTPError) as exc:
-                    logger.warning(
-                        "Failed to archive run with ID %s: %s", run.run_id, exc
-                    )
+    trying_to_delete_datasets = set[int]()
+    deleted_datasets = set[int]()
+    deleted_storage_configs = set[int]()
+    deleted_git_repos = set[int]()
+    archived_runs = set[str]()
+    (
+        trying_to_delete_datasets,
+        archived_runs,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    ) = _handle_live_runs(
+        live_runs_by_dataset,
+        datasets,
+        one_week_ago,
+        trying_to_delete_datasets,
+        archived_runs,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    )
+    (
+        trying_to_delete_datasets,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    ) = _handle_archived_runs(
+        archived_runs_by_dataset,
+        datasets,
+        one_week_ago,
+        trying_to_delete_datasets,
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+    )
 
-            _delete_dataset(dataset_id, dataset.storage_config)
+    logger.info(
+        "Deleted %s datasets, %s storage configs, %s git repos and archived %s runs",
+        deleted_datasets,
+        deleted_storage_configs,
+        deleted_git_repos,
+        archived_runs,
+    )
+    if trying_to_delete_datasets != deleted_datasets:
+        logger.warning(
+            "Tried to delete %s datasets, but only deleted %s datasets",
+            trying_to_delete_datasets,
+            deleted_datasets,
+        )
 
 
 if __name__ == "__main__":
